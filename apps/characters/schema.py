db444aa1d258fac685766ff4428d4048907eb41b
Wilbert <w.kreativo@gmail.com>
2025-06-24 21:46:27 -0500

    Refactor Character schema: add filtering with DjangoFilterConnectionField, convert to Relay structure, and update CreateCharacter mutation.

--- apps/characters/schema.py
+++ apps/characters/schema.py
@@ -1,27 +1,41 @@
+import django_filters
 import graphene
+from graphene import relay
+from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
 from apps.movies.models import Movie
 from .models import Character
 
 
+class CharacterFilter(django_filters.FilterSet):
+    class Meta:
+        model = Character
+        fields = {
+            'name': ['icontains'],
+            'birth_year': ['exact'],
+            'gender': ['exact'],
+        }
+
+
 class CharacterType(DjangoObjectType):
     class Meta:
         model = Character
+        interfaces = (relay.Node,)
         fields = "__all__"
 
 
 class Query(graphene.ObjectType):
-    all_characters = graphene.List(CharacterType, name=graphene.String())
+    node = relay.Node.Field()
 
-    def resolve_all_characters(self, info, name=None):
-        if name:
-            return Character.objects.filter(name__icontains=name)
-        return Character.objects.all()
+    all_characters = DjangoFilterConnectionField(
+        CharacterType,
+        filterset_class=CharacterFilter,
+    )
 
 
-class CreateCharacter(graphene.Mutation):
-    class Arguments:
+class CreateCharacter(relay.ClientIDMutation):
+    class Input:
         name = graphene.String(required=True)
         birth_year = graphene.String()
         gender = graphene.String()
@@ -29,7 +43,13 @@ class CreateCharacter(graphene.Mutation):
 
     character = graphene.Field(CharacterType)
 
-    def mutate(self, info, name, birth_year=None, gender=None, movie_ids=None):
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        name = input.get('name')
+        birth_year = input.get('birth_year')
+        gender = input.get('gender')
+        movie_ids = input.get('movie_ids')
+
         character = Character.objects.create(
             name=name,
             birth_year=birth_year,
